2bc58bec77bfb95786d6fdd880dbbc6b6198b206
ThuanDoVan <thuandv.bkit@gmail.com>
2021-08-15 19:32:53 +0700

    Add the Script and demo file

---
+++ Assets/TTS/Demo/GUIDemo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIDemo : MonoBehaviour {
+  // private string textToSpeech = "EDIT ME";
+  // void OnGUI() {
+  //
+  //   GUI.Box(new Rect(Screen.width / 2 - 250, Screen.height / 2 - 400, 500, 800), "TTS");
+  //   textToSpeech = GUI.TextField(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 250, 400, 100),textToSpeech,25);
+  //   if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 200, 50),"Speech")) {
+  //     TextToSpeechManager.Instance.PlayTextToSpeech(textToSpeech);
+  //   }
+  //   
+  // }
+
+  public InputField inputField;
+
+  public void OnSpeech() {
+    if (!string.IsNullOrEmpty(inputField.text)) {
+      TextToSpeechManager.Instance.PlayTextToSpeech(inputField.text);
+    }
+  }
+}
---
+++ Assets/TTS/TextToSpeechManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(AudioSource))]
+public class TextToSpeechManager : MonoBehaviour {
+
+  public static TextToSpeechManager Instance;
+  private AudioSource audioSource;
+  private const string API_CALL =
+    "https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q={0}&tl=En-gb";
+  // private Dictionary<string,AudioClip> cachedSound = new Dictionary<string,AudioClip>();
+  private HashSet<string> playingSound = new HashSet<string>();
+
+  public const string SAVE_SOUND_DIR = "TTSSOUNDCACHED";
+
+  private string LocalSoundFilePath(string text) {
+    return Path.Combine(Application.persistentDataPath, SAVE_SOUND_DIR, $"{text}.mp3");
+  }
+
+  private string LocalSoundDirPath => Path.Combine(Application.persistentDataPath, SAVE_SOUND_DIR);
+  private void Awake() {
+    if (Instance == null) {
+      Instance = this;
+      DontDestroyOnLoad(this.gameObject);
+    } else {
+      Destroy(this.gameObject);
+    }
+
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  public void PlayTextToSpeech(string text) {
+    text = text.ToLower();
+    text.TrimEnd(' ');
+    text.TrimStart(' ');
+    if (playingSound.Contains(text)) {
+      return;
+    } /*else if(cachedSound.ContainsKey(text)) {
+      audioSource.PlayOneShot(cachedSound[text]);
+    }*/ else {
+      StartCoroutine(GetAudioClip(text));
+    }
+  }
+  
+  
+  IEnumerator GetAudioClip(string text) {
+    var isLocal = IsAudioClipExistLocal(text);
+    var uri = string.Format(API_CALL,text);
+    if (isLocal) {
+      uri = new Uri(LocalSoundFilePath(text)).AbsoluteUri;
+    }
+    Debug.Log("LoadFileFromPath: " + uri);
+    using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, AudioType.MPEG)) {
+      playingSound.Add(text);
+      yield return www.SendWebRequest();
+
+      if (www.isHttpError || www.isNetworkError)
+      {
+        Debug.LogError(www.error);
+        playingSound.Remove(text);
+
+      }
+      else {
+        if(!isLocal)
+          SaveAudioClipToLocal(text, www);
+        AudioClip myClip = DownloadHandlerAudioClip.GetContent(www);
+        audioSource.PlayOneShot(myClip);
+        playingSound.Remove(text);
+      }
+    }
+  }
+
+  private bool IsAudioClipExistLocal(string text) {
+    return File.Exists(LocalSoundFilePath(text));
+  }
+
+  private void SaveAudioClipToLocal(string text, UnityWebRequest requestedData) {
+    if (!Directory.Exists(LocalSoundDirPath)) {
+      Directory.CreateDirectory(LocalSoundDirPath);
+    }
+    Debug.Log("SaveFileToPath: " + LocalSoundFilePath(text));
+    System.IO.File.WriteAllBytes (LocalSoundFilePath(text), requestedData.downloadHandler.data);
+  }
+}
